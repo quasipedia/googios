3b78011f816101167917e84f4fe45b4bfdddbe4c
Mac Ryan <quasipedia@gmail.com>
2014-12-07 03:09:01 +0100

    Wizard is now complete

--- googios/strings.py
+++ googios/strings.py
@@ -41,17 +41,16 @@ timeout).  The cache should however be explicitly update with a cron job
 `googios update`.
 ''',
 
-    'cache.min_end':
+    'cache.back':
 '''
 By default the cache contains all shifts from the one active at the time of
 cache creation to the last available on Google.  You can override this by
-inserting the *minimum ending time* and the *maximum starting time* of the
-shifts that should be cached.  Both values should be ISO-formatted
-(example: 2014-12-01T08:59:00+01:00).  Insert the minimum end.
+selecting the numbers of days in both past and future that you would like to
+be cached.  Let's start from the past...
 ''',
 
-    'cache.max_start':
-'''Insert the maximum start.''',
+    'cache.forward':
+'''...and now the future.''',
 
     'cache.directory':
 '''
@@ -62,7 +61,7 @@ named `<roster-name>.cache`, so be mindful not to call two rosters with the
 same name, if you are using the same directory for their cache.
 ''',
 
-    'fallback.mail':
+    'fallback.email':
 '''
 Sometimes it may be impossible to retrieve the contact details of a person,
 or there might be a period of time for which nobody has been assigned.  Enter
@@ -84,15 +83,22 @@ be called `<roster-name>.log`
 ''',
 
     'log.level':
-'''Select what minimum log level you would like to generate messages for.''',
+'''
+Select what minimum log level you would like to generate messages for.
+Possible choices are DEBUG, INFO, WARNING, ERROR, CRITICAL
+''',
 
     'done':
 '''
-Congratulations!  GooGios is now ready.  These are the first few lines of
-your newly cerated {} roster.  The entire cache for the dates between {} and
-{} contains {} shifts.
+Congratulations!  GooGios is now ready.  You can now test that the
+configuration works by issuing:
 
-Don't forget to add the line `googios update {}` to your crontab!
+    googios cache
+
+************************************************************************
+Also: do not forget to add the cache-downloading job to your crontab!!!!
+    `googios update {} --dir={}`
+************************************************************************
 ''',
 
 }
--- googios/wizard.py
+++ googios/wizard.py
@@ -9,20 +9,80 @@ import os
 import string
 import logging
 
+import pytz
+import dateutil.parser
+
 from strings import STRINGS
-from utils import get_calendar_service, first_day_of_previous_month
+from utils import get_calendar_service
 from calendars import get_available_calendars
 
 
 DEFAULTS = {
     'time_shift': 0,
     'cache.timeout': 10,
-    'cache.min_end': first_day_of_previous_month().isoformat(),
-    'cache.max_start': None,
+    'cache.back': 0,
+    'cache.forward': None,
     'cache.directory': os.getcwd(),
     'log.directory': os.getcwd(),
     'log.level': 'INFO',
 }
+
+
+def validate_positive(string_):
+    '''Validation for integers'''
+    try:
+        ret = int(string_)
+    except ValueError:
+        return False
+    return ret if ret >= 0 else False
+
+
+def validate_simple_string(string_):
+    '''Validate a string of only ASCII, digits and -._'''
+    allowed = string.ascii_letters + string.digits + '_-.'
+    if set(string_).issubset(allowed):
+        return string_
+    return False
+
+
+def validate_datetime(string_):
+    '''Validations for dates/times.'''
+    try:
+        date = dateutil.parser.parse(string_)
+    except ValueError:
+        return False
+    return date.replace(tzinfo=pytz.UTC).isoformat()
+
+
+def validate_directory(string_):
+    '''Validation for directories.'''
+    try:
+        path = os.path.realpath(string_)
+    except ValueError:
+        print('That does not look like a valid path')
+        return False
+    if not os.path.exists(path):
+        print('The directory does not exist')
+        return False
+    if not os.access(path, os.W_OK | os.X_OK):
+        print('The directory is not writable')
+        return False
+    return path
+
+
+def validate_log_level(string_):
+    string_ = string_.upper()
+    if string_ in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+        return string_
+    return False
+
+
+def trust(string_):
+    '''Skip validation entirely, but output a message.'''
+    print('\n***********************************************************')
+    print('I will trust you on that one, as it is too complex to check!')
+    print('***********************************************************')
+    return string_
 
 
 def ask(string_id, question=None, validator=None, msg_args=()):
@@ -34,9 +94,11 @@ def ask(string_id, question=None, validator=None, msg_args=()):
         question = '{} [{}]'.format(question, DEFAULTS[string_id])
     while True:
         answer = raw_input(question + '  ').strip()
-        if validator(answer):
-            return answer
-        if string_id in DEFAULTS and answer == '':
+        if answer:
+            validated = validator(answer)
+            if validated is not False:
+                return validated
+        if not answer and string_id in DEFAULTS:
             return DEFAULTS[string_id]
 
 
@@ -51,22 +113,16 @@ def pick_calendar():
         lines.append('{:<3}: {}'.format(counter, description))
     msg_args = '\n'.join(lines)
     question = 'Pick a number between 1 and {}'.format(counter)
-    validator = lambda x: x in choices
+    validator = lambda x: x if x in choices else False
     selected = ask('cid', question, validator, [msg_args])
     return choices[selected]
-
-
-def pick_name():
-    '''Return the human-friendly name for the roster.'''
-    allowed = string.ascii_letters + string.digits + '_-.'
-    return ask('name', 'Choose a name', lambda x: all(l in allowed for l in x))
 
 
 def pick_time_shift():
     '''Ruturn the time-shift for the roster.'''
     allowed = list(map(str, range(24)))
-    return int(ask('time_shift', 'Choose an integer value between 0 and 23',
-                   lambda x: x in allowed))
+    return ask('time_shift', 'Choose an integer value between 0 and 23',
+               lambda x: x if x in allowed else False)
 
 
 def wizard():
@@ -75,8 +131,27 @@ def wizard():
     ask('welcome')
     config = {
         'cid': pick_calendar(),
-        'name': pick_name(),
+        'name': ask('name', 'Choose a name', validate_simple_string),
         'time_shift': pick_time_shift(),
+        'cache.timeout': ask('cache.timeout',
+                             'Choose an integer amount of minutes',
+                             validate_positive),
+        'cache.back': ask('cache.back', 'Choose an integer number of days',
+                          validate_positive),
+        'cache.forward': ask('cache.forward',
+                             'Choose an integer number of days',
+                             validate_positive),
+        'cache.directory': ask('cache.directory', 'Choose a directory',
+                               validate_directory),
+        'fallback.email': ask('fallback.email', 'Enter a valid email address',
+                              trust),
+        'fallback.phone': ask('fallback.phone',
+                              'Enter a valid telephone number',
+                              trust),
+        'log.directory': ask('log.directory', 'Choose a directory',
+                             validate_directory),
+        'log.level': ask('log.level', 'Choose a level', validate_log_level),
     }
+    ask('done', msg_args=[config['name'], os.getcwd()])
     logging.disable(logging.NOTSET)
     return config
